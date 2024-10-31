b5662b53393d4e032b7c53cd52f0330109bccd70
shlomoShalem <s0552288304@gmail.com>
2024-10-31 12:21:35 +0200

    added components

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import AddMission from "./components/AddMission";
+import MissionsList from "./components/MissionsList";
+
+export default function App() {
+  return (
+    <div>
+      <AddMission/>
+      <MissionsList/>
+    </div>
+  )
+}
